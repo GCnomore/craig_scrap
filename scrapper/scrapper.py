32978549157641e8088d7d757c848cf92e710577
isaac <isaac@spigen.com>
2023-10-20 07:39:48 -0700

    beautifulsoup

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,26 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+
 from config.app_config import AppConfig
 
 class Scrapper:
     config: AppConfig = None
-    
+
     def __init__(self, config):
         self.config = config
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.wait = WebDriverWait(self.driver, 10)
     
     def load_browser(self, url):
         if(url):
             self.driver.get(url)
-            sleep(5)
+            self.wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="search-results-page-1"]/ol')))
             
             if(self.config['filter'] == 'filter'):
                 self.set_filters()
             else:
                 self.start()
+
+    def locate_listing_container(self):
+        self.list_container = self.driver.find_element(By.XPATH, '//*[@id="search-results-page-1"]/ol')
+
+    def get_html_from_target(self, target):
+        return BeautifulSoup(target.get_attribute('outerHTML'), 'html.parser')
                 
     def set_filters(self):
         if(self.config['hide_dup']):
@@ -40,7 +51,7 @@ class Scrapper:
             
             miles_input.send_keys(self.config['miles_from_location'])
             zip_input.send_keys(self.config['zip_code'])
-            
+
             sleep(1)
             
             apply_btn.click()
@@ -48,6 +59,10 @@ class Scrapper:
             sleep(5)
     
     def start(self):
+        self.locate_listing_container()
+        soup = self.get_html_from_target(self.list_container)
+        sleep(5)
+        # print(soup.li)
         pass
     
     def quit(self):
